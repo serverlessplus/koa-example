1feda25f05c3b2bf30f5a235de789fe4825531b5
ritchiechen <ritchiechen@tencent.com>
2019-04-11 22:36:35 +0800

    add middleware

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 'use strict';
 
 const Koa = require('koa');
+const getEventAndContext = require('serverlessplus/middleware').getEventAndContext;
 const app = new Koa();
 
+app.use(getEventAndContext);
+
 app.use(async ctx => {
+    console.log(ctx.payload.event);
+    console.log(ctx.payload.context);
     ctx.body = 'Hello World';
 });
 
